f47370f9ea4a79d3b5f3cdecee3dca5c6005a09f
rbokel <rbokel.github@qlod.org>
2016-12-01 12:53:38 +0100

    move to corespring

---
+++ docs/demo/controllers/entry.js
@@ -0,0 +1,2 @@
+exports['corespring-number-line-ng15'] = require('corespring-number-line-ng15-controller');
+exports['corespring-number-line-ng15'].version =  '../node_modules/corespring-number-line-ng15/controller';
---
+++ src/demo.js
@@ -0,0 +1,3 @@
+import Element from './index.js';
+
+document.registerElement('corespring-number-line-ng15', Element);
