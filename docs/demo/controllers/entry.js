a9aec808bed2db7bc12d25534fdf9d8c9e4347fc
Ed Eustace <ed.eustace@gmail.com>
2017-02-09 09:55:58 +0000

    rm redundant files

--- docs/demo/controllers/entry.js
+++
@@ -1,2 +0,0 @@
-exports['corespring-number-line-ng15'] = require('corespring-number-line-ng15-controller');
-exports['corespring-number-line-ng15'].version =  '../node_modules/corespring-number-line-ng15/controller';
